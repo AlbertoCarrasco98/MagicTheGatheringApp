845ce51beda54b5274f98ba49957362a75ebae75
Alberto Carrasco del Cid <albertocarrasco.dc@gmail.com>
2025-01-10 12:59:44 +0100

    Initial Commit

---
+++ MagicTheGathering/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MagicTheGathering
+//
+//  Created by Juan Antonio Carrasco del Cid on 10/1/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
